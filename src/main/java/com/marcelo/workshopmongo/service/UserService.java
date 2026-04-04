044a3ee1f87a559dc8c3d64c4c20a742b1c3d586
Cyteee <coyotemalandro@gmail.com>
2026-04-03 23:45:38 -0300

    .

--- src/main/java/com/marcelo/workshopmongo/service/UserService.java
+++ src/main/java/com/marcelo/workshopmongo/service/UserService.java
@@ -4,9 +4,11 @@ import com.marcelo.workshopmongo.domain.User;
 import com.marcelo.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService {
 
     @Autowired
