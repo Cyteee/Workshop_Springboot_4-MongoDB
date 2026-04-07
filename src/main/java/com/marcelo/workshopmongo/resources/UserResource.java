7c9ff4779329d0066fe9fc5c7ef785383a86e921
Cyteee <coyotemalandro@gmail.com>
2026-04-07 00:04:03 -0300

    .

--- src/main/java/com/marcelo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marcelo/workshopmongo/resources/UserResource.java
@@ -6,10 +6,7 @@ import com.marcelo.workshopmongo.service.UserService;
 import com.marcelo.workshopmongo.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -43,5 +40,11 @@ public class UserResource {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId().toUri());
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
